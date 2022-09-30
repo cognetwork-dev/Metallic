95dfcfdfcc466393f466acc3f372f11a873fd3a1
David Reed <reedswork@proton.me>
2022-09-29 21:15:39 -0400

    use commonJS bare-server-node, add as dev dependency

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,17 @@
+const createBareServer = require("@tomphttp/bare-server-node");
+
 /**
  *
  * @param {import('express').Express} app
  */
 function setupProxy(app) {
-  try {
-    const bareServer = import("@tomphttp/bare-server-node").then(
-      ({ default: createBareServer }) => createBareServer("/bare/")
-    );
+  const bareServer = createBareServer("/bare/");
 
-    app.use(async (req, res, next) => {
-      if ((await bareServer).shouldRoute(req)) {
-        (await bareServer).routeRequest(req, res);
-      } else next();
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  app.use((req, res, next) => {
+    if (bareServer.shouldRoute(req)) {
+      bareServer.routeRequest(req, res);
+    } else next();
+  });
 }
 
 module.exports = setupProxy;
