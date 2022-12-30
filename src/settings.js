2beb2e7ad9c27b871197d29d3775505d5d25b496
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2022-12-29 16:45:09 -0800

    Change rounding

--- src/settings.js
+++ src/settings.js
@@ -10,6 +10,7 @@ export var useLocalBackground = () => useLocalFallback("background", "none");
 export var useLocalAppearance = () => useLocalFallback("appearance", "default");
 export var useLocalControls = () => useLocalFallback("controls", "default");
 export var useLocalWindow = () => useLocalFallback("window", "default");
+export var useLocalRounding = () => useLocalFallback("rounding", "default");
 export var useLocalProxy = () => useLocalFallback("type", "Ultraviolet");
 export var useLocalTitle = () => useLocalStorage("title");
 export var useLocalIcon = () => useLocalStorage("icon");
