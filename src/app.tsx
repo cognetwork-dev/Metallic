a3e461cc0a403239fd177bc3985fa0af2ba89758
Nebelung <nebelung@mailfence.com>
2024-01-03 02:54:53 +0000

    New Preact project with Vite

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <>
+      <h1>Metallic</h1>
+    </>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import { render } from 'preact'
+import { App } from './app.tsx'
+
+render(<App />, document.getElementById('app')!)
