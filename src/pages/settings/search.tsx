6a4377e4109819643a93974c42a6e66aee16a05f
Nebelung <nebelung@mailfence.com>
2024-01-15 22:48:57 +0000

    Prevent dragging of images

--- src/pages/settings/search.tsx
+++ src/pages/settings/search.tsx
@@ -22,15 +22,15 @@ function SearchSettings() {
             <h1 class="text-4xl font-bold mb-8">Service</h1>
             <section class="flex flex-wrap items-center gap-3">
                 <SquareButton active={service == "ultraviolet"} onClick={() => setService("ultraviolet")}>
-                    <img height="24" width="24" src={ultravioletLogo} loading="lazy" />
+                    <img draggable={false} height="24" width="24" src={ultravioletLogo} loading="lazy" />
                     <span class="font-bold">Ultraviolet</span>
                 </SquareButton>
                 <SquareButton active={service == "dynamic"} onClick={() => setService("dynamic")}>
-                    <img height="24" width="24" src={dynamicLogo} loading="lazy" />
+                    <img draggable={false} height="24" width="24" src={dynamicLogo} loading="lazy" />
                     <span class="font-bold">Dynamic</span>
                 </SquareButton>
                 <SquareButton active={service == "rammerhead"} onClick={() => setService("rammerhead")}>
-                    <img height="24" width="24" src={rammerheadLogo} loading="lazy" />
+                    <img draggable={false} height="24" width="24" src={rammerheadLogo} loading="lazy" />
                     <span class="font-bold">Rammerhead</span>
                 </SquareButton>
                 <SquareButton active={service == "stomp"} onClick={() => setService("stomp")}>
