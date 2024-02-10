171c421b218a83999d3a8b64ec51d9d6ecd02865
Nebelung <nebelung@mailfence.com>
2024-02-10 22:42:56 +0000

    Fix status codes

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-const pages = ["/"];
+const pages = ["/", "/apps", "/games", "/settings/search", "/settings/tab", "/settings/appearance", "/settings/locale"];
 const rammerhead = true;
 const github = "https://github.com/cognetwork-dev/Metallic";
 const discord = "https://discord.com/invite/yk33HZSZkU";
